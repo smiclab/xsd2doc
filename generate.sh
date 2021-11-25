b8c4e2c700b1c172f3be22fd05d1095f2d3c3857
Jaehyeon Lee <jhlee@smic.kr>
2021-11-25 19:03:07 +0900

    add links
    Signed-off-by: Jaehyeon Lee <jhlee@smic.kr>

--- generate.sh
+++ generate.sh
@@ -4,9 +4,13 @@ INPUT_DIR=xsd
 OUTPUT_DIR=docs
 XSLT_FILE=xs3p/xs3p.xsl
 
-for f in $INPUT_DIR/*.xsd
+cp $XSLT_FILE $INPUT_DIR/.
+
+cd $INPUT_DIR
+for f in *.xsd
 do
-  FILENAME=`basename $f`
-  echo "Generating documentation for: $FILENAME"
-  xsltproc --nonet --output $OUTPUT_DIR/$FILENAME.html $XSLT_FILE $f
+  echo "Generating documentation for: $f"
+  xsltproc --param linksFile "'links.xml'" --param searchImportedSchemas "'true'" --nonet --output ../$OUTPUT_DIR/$f.html xs3p.xsl $f
 done
+
+rm xs3p.xsl
